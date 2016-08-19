d72090c56f4b532a656e712f7d633e2b6d0af7d7
Marko Cupic <m.cupic@gmx.ch>
2016-08-20 00:00:53 +0200

    dev

---
+++ MarkocupicImportFromCsvBundle.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of Contao.
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+namespace Markocupic\ImportFromCsvBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Configures the import-from-csv-bundle bundle.
+ *
+ * @author Marko Cupic <https://github.com/markocupic>
+ */
+class MarkocupicImportFromCsvBundle extends Bundle 
+{
+}
