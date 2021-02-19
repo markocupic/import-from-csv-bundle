bf1fcd6133ab632e2793859f471234ed6efd6783
Marko Cupic <m.cupic@gmx.ch>
2021-02-19 21:06:06 +0100

    Fixed phpdoc in the head

--- src/Resources/contao/dca/tl_import_from_csv.php
+++ src/Resources/contao/dca/tl_import_from_csv.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Import From CSV Bundle.
  *
- * (c) Marko Cupic 19.02.2021 <m.cupic@gmx.ch>
+ * (c) Marko Cupic 2021 <m.cupic@gmx.ch>
  * @license MIT
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
