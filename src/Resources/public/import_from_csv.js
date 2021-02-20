2706a7d0b758ef77b6478ce6f637bf844b3ec87f
Marko Cupic <m.cupic@gmx.ch>
2021-02-20 15:29:56 +0100

    Clean up css and javascript

--- src/Resources/public/import_from_csv.js
+++ src/Resources/public/import_from_csv.js
@@ -1,43 +1,40 @@
-window.addEvent('domready', function () {
+document.addEventListener("DOMContentLoaded", function (event) {
+    if (document.querySelector('.import-report-table')) {
 
-    if (document.id('reportTable')) {
-
-        if (document.querySelector('h2.sub_headline')) {
-            $$('h2.sub_headline')[0].setStyle('display', 'none');
-        }
-
-        if (document.querySelector('.tl_message')) {
-            $$('.tl_message')[0].setStyle('display', 'none');
+        let elements = document.querySelectorAll('h2.sub_headline, .tl_message, .tl_formbody_submit');
+        if (elements) {
+            elements.forEach((el) => {
+                el.style.display = 'none'
+            });
         }
 
         // change href property of the header_back link
-        if (document.querySelector('a.header_back')) {
-            $$('a.header_back')[0].setProperty('href', window.location);
+        let link = document.querySelector('a.header_back');
+        if (link) {
+            link.setAttribute('href', window.location);
         }
 
+        // Use button to show failed inserts only
+        let button = document.querySelector('.tl_submit.showErrorButton');
+        if (button) {
+            button.addEventListener('click', function (event) {
+                event.preventDefault();
+                let btn = event.target;
+                btn.classList.toggle('hideSuccess');
+                if (btn.classList.contains('hideSuccess')) {
+                    btn.innerText = btn.getAttribute('data-lbl-all')
+                } else {
+                    btn.innerText = btn.getAttribute('data-lbl-error-only')
+                }
+
+                let rows = document.querySelectorAll('tr.allOk');
+                if (rows) {
+                    rows.forEach((row) => {
+                        row.classList.toggle('hiddenRow');
+                    });
+                }
+            });
+        }
     }
 
-    // Use button to show failed inserts only
-    let button = document.querySelector('.tl_submit.showErrorButton');
-    if(button){
-        button.addEventListener('click', function(event){
-            event.preventDefault();
-            let btn = event.target;
-            btn.classList.toggle('hideSuccess');
-            if(btn.classList.contains('hideSuccess'))
-            {
-                btn.innerText = btn.getAttribute('data-lblall')
-            }else{
-                btn.innerText = btn.getAttribute('data-lblerroronly')
-            }
-
-            let rows = document.querySelectorAll('tr.allOk');
-            if(rows)
-            {
-                rows.forEach((row) => {
-                    row.classList.toggle('hiddenRow');
-                });
-            }
-        });
-    }
 });
