46c1c126078d925ebbc94f2a67493f2edf9b4b8b
Marko Cupic <m.cupic@gmx.ch>
2022-08-29 21:31:35 +0200

    Fix undefined array key issues

--- src/Resources/contao/dca/tl_import_from_csv.php
+++ src/Resources/contao/dca/tl_import_from_csv.php
@@ -54,7 +54,7 @@ $GLOBALS['TL_DCA']['tl_import_from_csv'] = [
             'delete'          => [
                 'href'       => 'act=delete',
                 'icon'       => 'delete.gif',
-                'attributes' => 'onclick="if (!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\')) return false; Backend.getScrollOffset();"',
+                'attributes' => 'onclick="if (!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\')) return false; Backend.getScrollOffset();"',
             ],
             'show'            => [
                 'href' => 'act=show',
