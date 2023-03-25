765aa1637eff044e4d45cac5d5ed9d1aa2d6f90f
Marko Cupic <marko.cupic@schule-ettiswil.ch>
2023-03-25 15:29:58 +0100

    Update php doc

--- public/js/importFromCsvApp.js
+++ public/js/importFromCsvApp.js
@@ -1,7 +1,7 @@
 /*
  * This file is part of Import From CSV Bundle.
  *
- * (c) Marko Cupic 2021 <m.cupic@gmx.ch>
+ * (c) Marko Cupic 2023 <m.cupic@gmx.ch>
  * @license MIT
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
--- src/MarkocupicImportFromCsvBundle.php
+++ src/MarkocupicImportFromCsvBundle.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of Import From CSV Bundle.
  *
- * (c) Marko Cupic 2022 <m.cupic@gmx.ch>
+ * (c) Marko Cupic 2023 <m.cupic@gmx.ch>
  * @license GPL-3.0-or-later
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
@@ -14,9 +14,7 @@ declare(strict_types=1);
 
 namespace Markocupic\ImportFromCsvBundle;
 
-use Markocupic\ImportFromCsvBundle\DependencyInjection\Compiler\AddSessionBagsPass;
 use Markocupic\ImportFromCsvBundle\DependencyInjection\MarkocupicImportFromCsvExtension;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MarkocupicImportFromCsvBundle extends Bundle
@@ -30,5 +28,4 @@ class MarkocupicImportFromCsvBundle extends Bundle
     {
         return new MarkocupicImportFromCsvExtension();
     }
-
 }
